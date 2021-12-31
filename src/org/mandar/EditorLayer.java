cd7ceea229bde43e1f0aac06d839a98641e057e4
Merouane Issad <logithss@gmail.com>
2021-12-31 17:17:22 -0500

    small imgui close change

--- src/org/mandar/EditorLayer.java
+++ src/org/mandar/EditorLayer.java
@@ -1,10 +1,18 @@
 package org.mandar;
 
 import imgui.ImGuiIO;
+import imgui.ImGuiStyle;
+import imgui.ImGuiViewport;
+import imgui.ImGuiWindowClass;
 import imgui.flag.ImGuiConfigFlags;
+import imgui.flag.ImGuiDockNodeFlags;
+import imgui.flag.ImGuiStyleVar;
+import imgui.flag.ImGuiWindowFlags;
 import imgui.gl3.ImGuiImplGl3;
 import imgui.glfw.ImGuiImplGlfw;
 import imgui.internal.ImGui;
+import imgui.internal.ImGuiDockNode;
+import imgui.type.ImBoolean;
 import org.mandar.core.GameEngine;
 import org.mandar.core.Input;
 import org.mandar.core.KeyCode;
@@ -50,11 +58,64 @@ public class EditorLayer extends Layer {
     public void onImGuiRender()
     {
         ImGui.showDemoWindow();
-        ImGui.begin("hello");
-        ImGui.text("a");
-        ImGui.sameLine();
-        ImGui.text("b");
+        /*boolean dockspaceOpen = true;
+        boolean opt_fullscreen_persistant = true;
+        boolean opt_fullscreen = opt_fullscreen_persistant;
+        ImGuiDockNode dockspace_flags = new ImGuiDockNode(0);
+        dockspace_flags.addLocalFlags(ImGuiDockNodeFlags.None);
+
+        ImGuiWindowClass window_flags = new ImGuiWindowClass();
+        window_flags.addDockNodeFlagsOverrideSet(ImGuiWindowFlags.MenuBar);
+        window_flags.addDockNodeFlagsOverrideSet(ImGuiWindowFlags.NoDocking);
+
+
+        if (opt_fullscreen)
+        {
+            ImGuiViewport viewport = ImGui.getMainViewport();
+            ImGui.setNextWindowPos(viewport.getPosX(), viewport.getPosY());
+            ImGui.setNextWindowSize(viewport.getSizeX(), viewport.getSizeY());
+            ImGui.setNextWindowViewport(viewport.getID());
+            ImGui.pushStyleVar(ImGuiStyleVar.WindowRounding, 0.0f);
+            ImGui.pushStyleVar(ImGuiStyleVar.WindowBorderSize, 0.0f);
+            window_flags.addDockNodeFlagsOverrideSet(ImGuiWindowFlags.NoTitleBar);
+            window_flags.addDockNodeFlagsOverrideSet(ImGuiWindowFlags.NoCollapse);
+            window_flags.addDockNodeFlagsOverrideSet(ImGuiWindowFlags.NoResize);
+            window_flags.addDockNodeFlagsOverrideSet(ImGuiWindowFlags.NoMove);
+            window_flags.addDockNodeFlagsOverrideSet(ImGuiWindowFlags.NoBringToFrontOnFocus);
+            window_flags.addDockNodeFlagsOverrideSet(ImGuiWindowFlags.NoNavFocus);
+        }
+
+        // When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background and handle the pass-thru hole, so we ask Begin() to not render a background.
+        if (window_flags.hasDockNodeFlagsOverrideSet(ImGuiDockNodeFlags.PassthruCentralNode))
+            window_flags.addDockNodeFlagsOverrideSet(ImGuiWindowFlags.NoBackground);
+
+        ImGui.pushStyleVar(ImGuiStyleVar.WindowPadding, 0);
+        ImGui.begin("DockSpace Demo", new ImBoolean(true), window_flags.geClassId());
+        ImGui.popStyleVar();
+
+        if (opt_fullscreen)
+            ImGui.popStyleVar(2);
+
+        ImGuiIO io = ImGui.getIO();
+        ImGuiStyle style = ImGui.getStyle();
+
+        float minWinSizeX = style.getWindowMinSize().x;
+        style.setWindowMinSize(370.0f, 370.0f);
+
+        if (io.hasConfigFlags(ImGuiConfigFlags.DockingEnable))
+        {
+            int dockspace_id = ImGui.getID("MyDockSpace");
+            ImGui.dockSpace(dockspace_id, 0, 0, dockspace_flags.getLocalFlags());
+        }
+
+        style.setWindowMinSize(minWinSizeX, minWinSizeX);
+
+        ImGui.begin("Viewport");
         ImGui.end();
+        ImGui.popStyleVar();
+        ImGui.end();*/
+
+
     }
 
     @Override
