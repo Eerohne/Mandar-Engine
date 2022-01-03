b7238528636399736485d34c65dc60e4c31dfda5
Merouane Issad <logithss@gmail.com>
2022-01-03 02:14:36 -0500

    Git root folder changed to add an app

--- Mandar-Editor/src/org/mandarEditor/EditorTest.java
+++ Mandar-Editor/src/org/mandarEditor/EditorTest.java
@@ -17,6 +17,8 @@ public class EditorTest {
             GameEngine e = new GameEngine(ENGINE_NAME, new TestLayer(), new EditorLayer());
             //GameEngine e = new GameEngine(ENGINE_NAME,new TestLayer());
             e.run();
+
+            Debug.coreLog("Editor was succesfully ported too!");
         } catch(Exception e){
             //Debug.coreError("{0} could not start", ENGINE_NAME); //<-- can't access the debugger if engine failed u dummy
             System.err.println(ENGINE_NAME + " could not start");
